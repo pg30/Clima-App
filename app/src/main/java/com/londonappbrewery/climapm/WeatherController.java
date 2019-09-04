57c182121cb9c7ed527ac8d33077ba364f68e4cd
Pranay Garg <pranaygarg001@gmail.com>
2019-09-05 01:21:37 +0530

    Networking Integrated

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -14,6 +14,15 @@ import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class WeatherController extends AppCompatActivity {
@@ -22,7 +31,7 @@ public class WeatherController extends AppCompatActivity {
     final int REQUEST_CODE = 123;
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+    final String APP_ID = "e72ca729af228beabd5d20e3b7749713";
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
@@ -84,6 +93,12 @@ public class WeatherController extends AppCompatActivity {
                 String longitude = String.valueOf(location.getLongitude());
                 Log.d("Clima", "latitude : "+latitude);
                 Log.d("Clima", "longitude : "+longitude);
+                //passing parameter to the api using imported dependency
+                RequestParams params = new RequestParams();
+                params.put("lat",latitude);
+                params.put("lon",longitude);
+                params.put("appid",APP_ID);
+                letsDoSomeNetworking(params);
             }
 
             @Override
@@ -133,7 +148,27 @@ public class WeatherController extends AppCompatActivity {
         }
     }
     // TODO: Add letsDoSomeNetworking(RequestParams params) here:
+    private void letsDoSomeNetworking(RequestParams params)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
 
+        client.get(WEATHER_URL,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int StatusCode, Header[] headers, JSONObject response)
+            {
+                Log.d("Clima","Success! JSON : "+response.toString());
+                //paste the JSON response on jsonmate.com to understand the fields and hierarchies
+                WeatherDataModel weatherData = WeatherDataModel.fromJSON(response);
+            }
+            @Override
+            public void onFailure(int StatusCode, Header[] headers,Throwable e,JSONObject response)
+            {
+                Log.e("Clima","Fail : "+e.toString());
+                Log.d("Clima","StatusCode : "+StatusCode);
+                Toast.makeText(WeatherController.this,"Request Failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
     // TODO: Add updateUI() here:
